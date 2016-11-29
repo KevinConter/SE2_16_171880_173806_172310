590372564ad73baba25c70e0d0dfe1e12b8d651d
root <root@localhost.localdomain>
2016-11-29 15:37:48 +0100

    Aggiunta gestione piatto e modifiche a gestione utente

--- db.js
+++ db.js
@@ -18,6 +18,16 @@ var piatti = [];
   * @param {optional Array[String]} allergeni una lista di allergeni. Può essere omessa
 */
 function User(nome,cognome,data,mail,password,via,recapito,allergeni = []){
+	if(typeof nome != 'string' || 
+		typeof cognome != 'string' || 
+		! (data instanceof Date) || 
+		typeof mail != 'string' || 
+		typeof password != 'string' || 
+		typeof via != 'string' || 
+		!(typeof recapito == 'string' || (recapito instanceof Array)) || 
+		!(allergeni instanceof Array)){
+			return null;
+	}
 	this.nome = nome;
 	this.cognome = cognome;
 	this.data_nascita = data;
@@ -29,17 +39,20 @@ function User(nome,cognome,data,mail,password,via,recapito,allergeni = []){
 
 	this.toString = function(){
 		return 'nome: '+this.nome+'\n' +
-		'cognome: '+this.cognome+'\n'+
-		'via: '+this.via+'\n'+
-		'recapito: '+ this.recapito+'\n'+
-		'allergeni: '+ this.allergeni;
+			'cognome: '+this.cognome+'\n'+
+			'E-mail: '+this.mail+'\n'+
+			'via: '+this.via+'\n'+
+			'recapito: '+ this.recapito+'\n'+
+			'allergeni: '+ this.allergeni;
 	};
 }
 
 /**
-  * Variabile per la comparazione tra gli utenti
+  * Comparatore tra utenti
+  * @param {User} u1
+  * @param {User} u2
 */
-var UserComparator = function(u1,u2){
+function UserComparator(u1,u2){
 	if(u1 instanceof User && u2 instanceof User){
 		return u1.id - u2.id;
 	}
@@ -52,7 +65,7 @@ var UserComparator = function(u1,u2){
   * @return {User} ritorna l'utente con l'id passato come parametro, oppure null se non c'è
 */
 function cercaUtente(id){
-	for (var i=0 ; i<users.length ; i++ ){
+	for (var i in users){
 		if(users[i] != undefined && i == id){
 			return users[i];
 		}
@@ -68,7 +81,7 @@ function cercaUtente(id){
   * @return {User} ritorna l'utente con quella mail e password, null se non c'è oppure la mail o password sono sbagliate
 */
 function cercaUtente(mail,password){
-	for(var i=0 ; i<users.length ; i++){
+	for(var i in users){
 		var u = users[i]
 		if(u != undefined){
 			if(u.mail.localeCompare(mail) == 0 && u.password.localeCompare(password) == 0){
@@ -85,7 +98,7 @@ function cercaUtente(mail,password){
   * @return {Boolean} false se la mail è già presente, false altrimenti
 */
 function chechMail(mail){
-	for (var i=0 ; i<users.length ; i++ ){
+	for (var i in users){
 		if(users[i].mail.localeCompare(mail) == 0){
 			return false;
 		}
@@ -96,7 +109,7 @@ function chechMail(mail){
 /**
   * Funzione che aggiunge al vettore l'utente passato come parametro
   * @param {User} u l'utente da aggiungere
-  * @return {Number} l'id associato all'utente aggiunto | undefined se non è un utente
+  * @return {Number} l'id associato all'utente aggiunto | undefined se l'inserimento non è andato a buon fine
 */
 function addUser(u){
 	if(u instanceof User){
@@ -133,4 +146,111 @@ function aggiungiAllergie(id,allergie){
 	}
 }
 
+/********************
+ * Gestione piatti  *
+ ********************/
 
+/**
+  * Costruttore di un piatto
+  * @param {String} nome il nome del piatto
+  * @param {String} ingredienti gli ingredienti del piatto
+  * @param {String} curiosita le curiosità del piatto
+  * @param {String} foto il percorso della foto del piatto
+  * @param {Array[String]} allergeni gli allergeni contenuti nel piatto
+*/
+function Piatto(nome,ingredienti,curiosita,foto,allergeni){
+	if(typeof nome != 'string' || 
+		typeof ingredienti != 'string' || 
+		typeof curiosita != 'string' || 
+		typeof foto != 'string' ||
+		!(allergeni instanceof Array)){
+		return null;
+	}
+	this.nome = nome;
+	this.ingredienti = ingredienti;
+	this.curiosita = curiosita;
+	this.foto = foto;
+	this.allergeni = allergeni;
+	
+	this.toString = function(){
+		return 'nome: '+this.nome+'\n'+
+			'ingredienti: '+this.ingredienti+'\n'+
+			'curiosita\': '+this.curiosita+'\n'+
+			'percorso foto: '+this.foto+'\n'+
+			'allergeni contenuti: '+this.allergeni;
+	}
+}
+
+/**
+  * Comparatore tra piatti
+  * @param {Piatto} p1
+  * @param {Piatto} p2
+*/
+function PiattoComparator(p1,p2){
+	if(p1 instanceof Piatto && 
+		p2 instanceof Piatto){
+		
+		return p1.nome.localeCompare(p2.nome);
+	}
+	return -1;
+}
+
+/**
+  * Funzione che controlla se il nome del piatto è già stato inserito
+  * @param {String} nome il nome da cercare
+  * @return {Boolean} true se il nome non è stato già inserito, false altrimenti
+*/
+function checkNomePiatto(nome){
+	for(var i in piatti){
+		var p = piatti[i];
+		if(p != undefined && p.nome.localeCompare(nome)==0){
+			return false
+		}
+	}
+	return true;
+}
+
+/**
+  * Funzione che aggiunge un piatto alla lista
+  * @param {Piatto} p piatto da aggiungere
+  * @return {Boolean} true se il piatto è stato inserito correttamente, false altrimenti
+*/
+function addPiatto(p){
+	if(p instanceof Piatto){
+		if(checkNomePiatto(p.nome)){
+			piatti.push(p);
+			return true;
+		}
+	}
+	return false;
+}
+
+/**
+  * Funzione che elimina un piatto
+  * @param {Piatto} p piatto da eliminare
+  * @return {Boolean} true se l'eliminazione è andata a buon fine, false altrimenti
+*/
+function deletePiatto(p){
+	if(p instanceof Piatto){
+		for(var i in piatti){
+			if(piatti[i].nome.localeCompare(p.nome)==0){
+				delete piatti[i];
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+/**
+  * Funzione che restituisce un piatto dato il nome
+  * @param {String} nome il nome del piatto da cercare
+  * @return {Piatto} il piatto con il nome dato, undefined se non è presente
+*/
+function getPiatto(nome){
+	for (var i in piatti){
+		if(piatti[i].nome.localeCompare(nome) == 0){
+			return piatti[i];
+		}
+	}
+}
