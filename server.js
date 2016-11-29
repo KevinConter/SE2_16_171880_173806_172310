205dce5d8fdfa87bac53610eb878e0e7c1c98bc7
Kevin Conter <KevinConter>
2016-11-29 16:18:41 +0100

    Inseriti i moduli per la creazione delle sessioni; spostato il file db.js; inserito il require per tale file.

--- server.js
+++ server.js
@@ -1,32 +1,37 @@
 /*
 	Server
 */
-/*************** LIBRERIES **********************/
+/*************** LIBRERIE **********************/
 var util = require('util');
 var express = require('express');
 var bodyParser = require('body-parser');
 var bind = require('bind');
 var session = require('express-session');
+var db = require('./moduli/db.js');
 /************************************************/
 
-/************** initialization ******************/
+/************** INIZIALIZZAZIONE ******************/
 var app = express();
 
-//set the server PORT
+//set della PORT del server
 app.set('port',( process.env.PORT || 8848));
-//set the server to respond to a file request
+//set il server per rispondere a richieste di file
 app.use('/files',express.static(__dirname+'/web'));
-//set Body-parser on the requests
+//applica body-parser alle richieste
 app.use(bodyParser.urlencoded({ extended: false }));
+//inizializzazione delle sessioni
+app.use(express.cookieParser());
+app.use(express.session({secret: "MySecretPassword"});
 /*************************************************/
 
-//Set the server to redirect a root request
+//Set del server per reindirizzare le richieste fatte alla root
 app.get("/",function(request,response){
 	//Controlli per verificare se esiste la sessione
 	//Se non esiste
 	response.redirect("/files/index.html");
 });
 
+//Bind per recuperare index.html
 app.get("/files/index.html",function(request,response){
 	var user={nome:"Asd",cognome:"qwery"};
 	bind.toFile("tpl/index.tpl",
@@ -37,6 +42,8 @@ app.get("/files/index.html",function(request,response){
 		}
 	);
 });
+
+
 
 
 app.listen(app.get('port'), function() {
