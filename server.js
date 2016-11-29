fb7b962c48646e5d12a7a01d1929f2fed3d515ab
DalRi Diego (ACC0085206) <diego.dalri@sci-a201pc49x.unitn.it>
2016-11-29 09:20:55 +0100

    Struttura base server.js

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 /*
 	Server
 */
+
+var util = require('util');
+var express = require('express');
+var parser = require('body-parser');
+var bind = require('bind');
+
+var app = express();
+
+app.set('port',( process.env.PORT || 8848));
+
+
+app.listen('8848','127.0.0.1');
+console.log('Server running at http://127.0.0.1:8848');
