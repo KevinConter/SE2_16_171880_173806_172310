fbec48d572d1097e04f95da4f2620cfc07cc42e2
Conter Kevin (ACC0083371) <kevin.conter@sci-a201pc48x.unitn.it>
2016-11-29 10:48:13 +0100

    Struttura base del server.

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 /*
 	Server
 */
-
+/*************** LIBRERIES **********************/
 var util = require('util');
 var express = require('express');
 var parser = require('body-parser');
 var bind = require('bind');
+var session = require('express-session');
+/************************************************/
 
+/************** initialization ******************/
 var app = express();
 
+//set the server PORT
 app.set('port',( process.env.PORT || 8848));
+//set the server to respond to a file request
 app.use('/files',express.static(__dirname+'/web'));
+//set Body-parser on the requests
+app.use(bodyParser.urlencoded({ extended: false }));
+/*************************************************/
+
+
 
 
 app.listen(app.get('port'), function() {
