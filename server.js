5e8298f84bef3ab7fe309229d6b8d2b2671b2adc
Diego Dal Ri <Diego Dal Ri>
2016-12-06 14:25:51 +0100

    L'oggetto della sessione non e' piu' l'utente ma il suo id

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get("/files/index.html",function(request,response){
 	/*var user={nome:"Nome",cognome:"Cognome"};
 	var dati=['asd','asd','asd'];*/
 	if(request.session.user){
-		var user = request.session.user;
+		var user = db.cercaUtenteId(request.session.user);
 		bind.toFile("tpl/index.tpl",
 			{
 			user: user
@@ -77,12 +77,16 @@ app.get("/GetDettagliPiatto",function(request,response){
 });
 
 app.get("/files/resoconto.html",function(request,response){
-	bind.toFile("tpl/resoconto.tpl",
-		{},
-		function(data){
-			response.writeHead(200,{"Content-Type":"text/html"});
-			response.end(data)
-		});
+	if(request.session.user){
+		bind.toFile("tpl/resoconto.tpl",
+			{},
+			function(data){
+				response.writeHead(200,{"Content-Type":"text/html"});
+				response.end(data)
+			});
+	}else{
+		response.redirect("/");
+	}
 });
 
 
@@ -139,76 +143,77 @@ app.post("/SignIn",function(request,response){
 		errore=true;
 	}
 	
-	if(errore){	
-		response.redirect("/");
-	}else{
-		//console.log("\nnome: "+nome+"\ncognome: "+cognome+"\nindirizzo: "+indirizzo+"\ndata: "+data+"\nrecapito: "+recapito+"\nmail: "+mail+"\npassword: "+pwd);
+	if(!errore){	
 		var user = new db.User(nome,cognome,indirizzo,new Date(data),recapito,mail,pwd,[]);
 		var id = db.addUser(user);
-		response.redirect("/");
 	}
+	response.redirect("/");
 });
 
 
 //per la modifica dell'utente
 app.post("/EditUser",function(request,response){
-	var errore=false;
-	var nome = undefined;
-	var cognome = undefined;
-	var indirizzo = undefined;
-	var data = undefined;
-	var recapito = undefined;
-	var mail = undefined;
-	var pwd = undefined;
-	var user = request.session.user;
-	
-	if(request.body.iNome){
-		nome = request.body.iNome;
-	}else{
-		errore=true;
-	}
-	
-	if(request.body.iCognome){
-		cognome = request.body.iCognome;
-	}else{
-		errore=true;
-	}
-	
-	if(request.body.iIndirizzo){
-		indirizzo = request.body.iIndirizzo;
-	}else{
-		errore=true;
-	}
-	
-	if(request.body.iData){
-		data = request.body.iData;
-	}else{
-		errore=true;
-	}
-	
-	if(request.body.iRecapito){
-		recapito = request.body.iRecapito;
-	}else{
-		errore=true;
-	}
-	
-	if(request.body.iMail){
-		mail = request.body.iMail;
-	}else{
-		errore=true;
-	}
-	
-	if(request.body.iPassword){
-		pwd = request.body.iPassword;
-	}else{
-		errore=true;
-	}
-	
-	if(errore){	
-		response.redirect("/");
-	}else{
-		if(user.mail!=mail){
-			if(db.checkMail(mail)){
+	if(request.session.user){
+		var errore=false;
+		var nome = undefined;
+		var cognome = undefined;
+		var indirizzo = undefined;
+		var data = undefined;
+		var recapito = undefined;
+		var mail = undefined;
+		var pwd = undefined;
+		var user = db.cercaUtenteId(request.session.user);
+		
+		if(request.body.iNome){
+			nome = request.body.iNome;
+		}else{
+			errore=true;
+		}
+		
+		if(request.body.iCognome){
+			cognome = request.body.iCognome;
+		}else{
+			errore=true;
+		}
+		
+		if(request.body.iIndirizzo){
+			indirizzo = request.body.iIndirizzo;
+		}else{
+			errore=true;
+		}
+		
+		if(request.body.iData){
+			data = request.body.iData;
+		}else{
+			errore=true;
+		}
+		
+		if(request.body.iRecapito){
+			recapito = request.body.iRecapito;
+		}else{
+			errore=true;
+		}
+		
+		if(request.body.iMail){
+			mail = request.body.iMail;
+		}else{
+			errore=true;
+		}
+		
+		if(request.body.iPassword){
+			pwd = request.body.iPassword;
+		}else{
+			errore=true;
+		}
+		
+		if(errore){	
+			response.redirect("/");
+		}else{
+			var b = true;
+			if(user.mail!=mail){
+				b = !db.checkMail(mail);
+			}
+			if(b){
 				user.nome=nome;
 				user.cognome=cognome;
 				user.data_nascita=data;
@@ -218,25 +223,15 @@ app.post("/EditUser",function(request,response){
 				user.recapito=recapito;
 				
 				db.updateUser(user);
-				request.session.user=user;
+				request.session.user=user.id;
 				response.redirect("/");
 			}else{
 				response.redirect("/files/error.html");
 			}
-		}else{
-			user.nome=nome;
-			user.cognome=cognome;
-			user.data_nascita=data;
-			user.mail=mail;
-			user.password=pwd;
-			user.via=indirizzo;
-			user.recapito=recapito;
 
-			db.updateUser(user);
-			request.session.user=user;
-			
-			response.redirect("/");
 		}
+	}else{
+		response.redirect("/");
 	}
 });
 
@@ -252,7 +247,7 @@ app.post("/LogIn",function(request,response){
 	}
 	if(mail != undefined && pwd != undefined){
 		var user = db.cercaUtenteMailPassword(mail,pwd);
-		request.session.user = user;
+		request.session.user = user.id;
 	}
 	response.redirect("/files/index.html");
 });
@@ -280,7 +275,7 @@ app.get("/files/editUser.html",function(request,response){
 			nome: user.nome,
 			cognome: user.cognome,
 			indirizzo: user.via,
-			data: user.data_nascita,
+			data: user.data_nascita..toJSON.substring(0,10),
 			recapito: user.recapito,
 			mail: user.mail,
 			password: user.password
@@ -327,7 +322,6 @@ app.post("/GetPiatti",function(request,response){
 app.get("/files/error.html",function(request,response){
 	var sess = request.session;
 	if(sess.user){
-		var user= db.cercaUtenteId(sess.user.id);
 		bind.toFile("tpl/error.tpl",
 		{
 			messaggio: "L'operazione ha causato un errore, ritenti l'operazione tra qualche minuto. Nel caso che l'errore persista contattare il team"
