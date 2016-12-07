309a08df547cc14ad0c197ec3089cab7a9eb6257
Bonani Damiano (ACC0083718) <damiano.bonani@sci-b106pc120x.unitn.it>
2016-12-07 15:29:23 +0100

    Creato il panel per la data nell'index al posto del datepiker

--- web/js/index.js
+++ web/js/index.js
@@ -1,3 +1,17 @@
+/**
+*	Imposta la data di oggi + 4 giorni
+*/
+function init(){
+	var data = new Date();
+	var giorni = ["Domenica","Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato"];
+	var mesi = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
+    data.setDate(data.getDate() +4);
+    document.getElementById("gg").innerHTML=giorni[data.getDay()];
+    document.getElementById("mm").innerHTML=mesi[data.getMonth()];
+ 	document.getElementById("numgg").innerHTML=data.getDate();
+	document.getElementById("yyyy").innerHTML=data.getFullYear();
+}
+
 
 /**
 *	Imposta la stringa che identifica il tipo di pulsante che è stato.
@@ -26,3 +40,4 @@ function salta(){
 		document.getElementById("formConferma").submit();
 	}
 }
+
